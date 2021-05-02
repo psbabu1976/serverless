adcb24d4f014ccda088b490f5465be1ce2c745f6
Suman <pulicheri.suman@broadcom.com>
2021-05-02 22:55:38 +0530

    changes

---
+++ server-local.js
@@ -0,0 +1,3 @@
+const app = require('./server/app');
+
+app.listen(3000, () => console.log('Local app listening on port 3000!'));
--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 let express = require('express');
 let serverless = require('serverless-http');
+let path = require('path');
+
 const { Router } = require('express');
 
 
@@ -13,7 +15,10 @@ router.get("/", (req,res)=>{
 app.use('/.netlify/functions/app', router);
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
-app.listen(5000, ()=>console.log('Running on 5000...'));
+console.log(__dirname);
+
+
+// app.listen(5000, ()=>console.log('Running on 5000...'));
 
 module.exports = app;
 module.exports.handler = serverless(app);
